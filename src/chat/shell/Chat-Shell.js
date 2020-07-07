df67d6be281d3017d16f8ccb6c7c9e2b94d8401d
Kanata <thunderpooh3@gmail.com>
2020-07-07 21:46:10 +0900

    Add files

---
+++ src/chat/shell/Chat-Shell.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ChatSearch from '../search/Chat-Search';
+
+import './Chat-Shell.css';
+
+function ChatSell() {
+  return (
+    <div id="chat-container">
+        <ChatSearch />
+    </div>
+  );
+}
+
+export default ChatSell;
